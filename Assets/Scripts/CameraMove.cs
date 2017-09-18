ad63202f76e1e7c4e92724724115cc6f336de0a5
MachumC <cameron.machum@ocsbstudent.ca>
2017-09-18 11:14:12 -0400

    Monday

---
+++ Assets/Scripts/CameraMove.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour {
+
+    public GameObject PLayer;
+
+    private Vector3 Offest;
+
+
+
+	// Use this for initialization
+	void Start () {
+
+        Offest = transform.position - PLayer.transform.position;
+
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+
+        transform.position = PLayer.transform.position + Offest; 
+
+	}
+}
